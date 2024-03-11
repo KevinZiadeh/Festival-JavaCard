7c93b52c1d9c9cbb7d32e36291022f4535b268fc
grs <grs@ostizan.eleves.polytechnique.fr>
2024-03-11 17:29:13 +0100

    unified code, decision on whether to use functions or calls directly in main

--- Client/actions_with_sig.py
+++ Client/actions_with_sig.py
@@ -20,10 +20,10 @@ GET_BALANCE = 0x10
 CREDIT = 0x20
 DEBIT = 0x30
 GENERATE_CARD_KEYS = 0x40
-READER_PUBKEY_MOD = 0x50
-READER_PUBKEY_EXP = 0x51
-READER_PUBKEY = 0x52
-CARD_PUBKEY_MOD = 0x60
+SEND_READER_PUBKEY_MOD = 0x50
+SEND_READER_PUBKEY_EXP = 0x51
+CREATE_CARD_READER_PUBKEY = 0x52
+GET_CARD_PUBKEY = 0x60
 CARD_PUBKEY_EXP = 0x61
 SEND_SIGNED_MSG = 0x70
 RECEIVE_SIGNED_MSG = 0x80
@@ -66,40 +66,31 @@ def retrieveReaderKeys():
         
     return reader_privKey, reader_pubKey
 
+
+
 def sendPubKeyMod(connection,mod):
     pubKeyMod = list(mod.to_bytes(128,byteorder='big'))
     Lc = len(pubKeyMod)
-    data, sw1, sw2 = connection.transmit([CLA, READER_PUBKEY_MOD, P1, P2, Lc] + pubKeyMod)
+    data, sw1, sw2 = connection.transmit([CLA, SEND_READER_PUBKEY_MOD, P1, P2, Lc] + pubKeyMod)
     return data, hex(sw1), hex(sw2)
 
 def sendPubKeyExp(connection,exp):
     pubKeyExp = list(mod.to_bytes(4,byteorder='big'))
     Lc = len(pubKeyExp)
-    data, sw1, sw2 = connection.transmit([CLA, READER_PUBKEY_EXP, P1, P2, Lc] + pubKeyExp)
+    data, sw1, sw2 = connection.transmit([CLA, SEND_READER_PUBKEY_EXP, P1, P2, Lc] + pubKeyExp)
     return data, hex(sw1), hex(sw2)
 
 def constructPubKey(connection,msg_hexlist):
     Lc = len(message_hexlist)
     message_hexlist = str.encode(message_hexlist)
-    data, sw1, sw2 = connection.transmit([CLA,READER_PUBKEY,P1,P2,Lc]+list(message_hexlist))
+    data, sw1, sw2 = connection.transmit([CLA,CREATE_CARD_READER_PUBKEY,P1,P2,Lc]+list(message_hexlist))
     return data,hex(sw1),hex(sw2)
+
+
 
 def getCardKey(connection):
     Lc = 0
-    data, sw1, sw2 = connection.transmit([CLA,GENERATE_CARD_KEYS,P1,P2,Lc])
-    return data,hex(sw1),hex(sw2)
-
-
-def getCardKeyMod(connection):
-    Lc = 0
-    response, sw1, sw2 = connection.transmit([CLA, CARD_PUBKEY_MOD, P1, P2, Lc])
-    response = int.from_bytes(response, 'big')
-    return response, hex(sw1), hex(sw2)
-
-def getCardKeyExp(connection):
-    Lc = 0
-    response, sw1, sw2 = connection.transmit([CLA, CARD_PUBKEY_EXP, P1, P2, Lc])
-    response = int.from_bytes(response, 'big')
+    response, sw1, sw2 = connection.transmit([CLA, GET_CARD_PUBKEY, P1, P2, Lc])
     return response, hex(sw1), hex(sw2)
 
 def constructCardKey(mod, exp):
@@ -110,6 +101,8 @@ def constructCardKey(mod, exp):
 def saveCardKey(pubKey):
     with open('card_pubKey.pem','wb') as file:
         file.write(pubKey.exportKey())
+        
+
         
 def signMessage(message_list):
     reader_privKey = RSA.importKey(open('reader_privKey.pem').read())
@@ -123,9 +116,9 @@ def sendSigned(connection, signedMsg):
     data, sw1, sw2 = connection.transmit([CLA, SEND_SIGNED_MSG, P1, P2, Lc] + list(signedMsg))
     return data, hex(sw1), hex(sw2)
 
-def receiveSigned(connection, signedMsg):
-    Lc = len(signedMsg)
-    response, sw1, sw2 = connection.transmit([CLA, RECEIVE_SIGNED_MSG, P1, P2, Lc] + signedMsg)
+def receiveSigned(connection):
+    Lc = 0
+    response, sw1, sw2 = connection.transmit([CLA, RECEIVE_SIGNED_MSG, P1, P2, Lc])
     return response, hex(sw1), hex(sw2)
 
 def verifySignature(message_list, signature):
@@ -139,7 +132,8 @@ def verifySignature(message_list, signature):
     except (ValueError, TypeError):
         return False
     
-    
+
+
 def main():
     connection = wait_for_card()
     print("Card Detected...")
@@ -157,7 +151,7 @@ def main():
                         
     valid = validate_pin(connection)
     
-    data, sw1, sw2 = connection.transmit([CLA, 0x60, 0x00, 0x00, 0x00])
+    data, sw1, sw2 = connection.transmit([CLA, GET_CARD_PUBKEY, 0x00, 0x00, 0x00])
     card_pub_mod_len = (data[0] << 8) + data[1]
     card_pub_mod = data[2:card_pub_mod_len+2]
     card_pub_exp_len =  (data[2 + card_pub_mod_len] << 8) + data[3 + card_pub_mod_len]
@@ -174,19 +168,19 @@ def main():
     n = list(readerPubKey.n.to_bytes(128,byteorder='big'))
     e = list(readerPubKey.e.to_bytes(128,byteorder='big'))
 
-    data, sw1, sw2 = connection.transmit([CLA, 0x50, 0x00, 0x00, len(n)] + n)
+    data, sw1, sw2 = connection.transmit([CLA, SEND_READER_PUBKEY_MOD, 0x00, 0x00, len(n)] + n)
     if sw1 != 0x90 or sw2 != 0x00:
         print('Error while transmitting modulus')
         print(hex(sw1), hex(sw2))
     else: 
         print('mod sent')
-    data, sw1, sw2 = connection.transmit([CLA, 0x51, 0x00, 0x00, len(n)] + e)
+    data, sw1, sw2 = connection.transmit([CLA, SEND_READER_PUBKEY_EXP, 0x00, 0x00, len(n)] + e)
     if sw1 != 0x90 or  sw2 != 0x00:
         print('Error while transmitting exponent')
         print(hex(sw1), hex(sw2))
     else: 
         print('exp sent')
-    data, sw1, sw2 = connection.transmit([CLA, 0x52, 0x00, 0x00])
+    data, sw1, sw2 = connection.transmit([CLA, CREATE_CARD_READER_PUBKEY, 0x00, 0x00])
     if sw1 != 0x90 or  sw2 != 0x00:
         print('Error while card creates key')
         print(hex(sw1), hex(sw2))
@@ -197,14 +191,14 @@ def main():
     msg_list = [ord(x) for x in message]
     signedMsg = signMessage(msg_list)
     
-    data, sw1, sw2 = connection.transmit([CLA, 0x70, 0x00, 0x00, len(signedMsg)] + list(signedMsg))
+    data, sw1, sw2 = connection.transmit([CLA, SEND_SIGNED_MSG, 0x00, 0x00, len(signedMsg)] + list(signedMsg))
     if sw1 != 0x90 or  sw2 != 0x00:
         print('Error while sending signed message')
         print(hex(sw1), hex(sw2))
     else: 
         print('Message signing and card side verification: ' + str(data))
     
-    data, sw1, sw2 = connection.transmit([CLA, 0x80, 0x00, 0x00, 0x00])
+    data, sw1, sw2 = connection.transmit([CLA, RECEIVE_SIGNED_MSG, 0x00, 0x00, 0x00])
     message = data[:-128]
     signature = data[-128:]
     # print("Message" + str(message))
